4e2a8c2084b42e8ba7211b93c645811a0ca2f891
Ian Overgard <ian.overgard@gmail.com>
2020-03-19 16:53:11 -0600

    Add a pretty basic halo effect (needs work), more detailed earth model, resize some controls

--- src/index.js
+++ src/index.js
@@ -11,11 +11,28 @@ const THREE = __importStar(require("three"));
 const OrbitControls_js_1 = require("three/examples/jsm/controls/OrbitControls.js");
 const GLTFLoader_1 = require("three/examples/jsm/loaders/GLTFLoader");
 const three_1 = require("three");
+/*
+    This is assuming a UVMap from the NASA blue-marble collection
+    mapped to a standard UV sphere.
+
+    This number might be approximate? It seems to roughly align with
+    where things should be, but I haven't looked closely into
+    specific projections etc.
+*/
+function sphereUVtoLatLon(uv) {
+    const lat = (uv.y - 0.5) * -180;
+    const lon = (uv.x - 0.5) * 360;
+    return new three_1.Vector2(lon, lat);
+}
 function main() {
-    const loader = new GLTFLoader_1.GLTFLoader();
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    // scene objects
+    const gltfLoader = new GLTFLoader_1.GLTFLoader();
     const cubeLoader = new THREE.CubeTextureLoader();
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.05, 1000);
+    const raycaster = new THREE.Raycaster();
+    let planetScene;
+    let mouse = new THREE.Vector2();
     const skybox = cubeLoader.load([
         'assets/skybox/right.png',
         'assets/skybox/left.png',
@@ -25,10 +42,20 @@ function main() {
         'assets/skybox/back.png',
     ]);
     scene.background = skybox;
-    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.75);
     scene.add(ambientLight);
     const directionalLight = new THREE.DirectionalLight(0xffffff, 1.5);
     scene.add(directionalLight);
+    // add a sphere for the atmosphere halo
+    let geometry = new THREE.SphereGeometry(0.975, 60, 30);
+    //let material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    let material = new THREE.ShaderMaterial({
+        vertexShader: document.getElementById('halo-vs').textContent,
+        fragmentShader: document.getElementById('halo-fs').textContent,
+        transparent: true
+    });
+    let sphere = new THREE.Mesh(geometry, material);
+    scene.add(sphere);
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.setClearColor('#001122');
@@ -37,16 +64,13 @@ function main() {
     camera.position.set(0, 0, 5);
     controls.update();
     directionalLight.position.copy(camera.position);
-    let planetScene;
-    loader.load('assets/earth.glb', (gltf) => {
+    gltfLoader.load('assets/earth.glb', (gltf) => {
         scene.add(gltf.scene);
         planetScene = gltf;
         console.log(scene);
     }, undefined, (errorEvent) => {
         console.error(errorEvent);
     });
-    var raycaster = new THREE.Raycaster();
-    var mouse = new THREE.Vector2();
     function onMouseMove(event) {
         mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
         mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
@@ -58,25 +82,24 @@ function main() {
         renderer.setSize(window.innerWidth, window.innerHeight);
     }
     window.addEventListener('resize', onWindowResize, false);
-    function uvToLatLon(uv) {
-        const lat = (uv.y - 0.5) * -180;
-        const lon = (uv.x - 0.5) * 360;
-        return new three_1.Vector2(lon, lat);
-    }
+    window.addEventListener('dblclick', (ev) => {
+        if (!planetScene) {
+            return;
+        }
+        var collisions = raycaster.intersectObjects(planetScene.scene.children, true);
+        if (collisions.length > 0) {
+            //just grab the first intersection
+            const hit = collisions[0];
+            const latLon = sphereUVtoLatLon(hit.uv);
+            document.getElementById('lon').value = latLon.x;
+            document.getElementById('lat').value = latLon.y;
+        }
+    });
     const animate = () => {
         requestAnimationFrame(animate);
         raycaster.setFromCamera(mouse, camera);
-        if (planetScene) {
-            var intersects = raycaster.intersectObjects(scene.children, true);
-            if (intersects.length > 0) {
-                //just grab the first intersection
-                const hit = intersects[0];
-                const latLon = uvToLatLon(hit.uv);
-                document.getElementById('lon').value = latLon.x;
-                document.getElementById('lat').value = latLon.y;
-            }
-        }
         controls.update();
+        directionalLight.position.copy(camera.position);
         renderer.render(scene, camera);
     };
     animate();
--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,30 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader, GLTF } from 'three/examples/jsm/loaders/GLTFLoader';
 import { Vector2 } from "three";
 
+/* 
+    This is assuming a UVMap from the NASA blue-marble collection
+    mapped to a standard UV sphere. 
+
+    This number might be approximate? It seems to roughly align with
+    where things should be, but I haven't looked closely into 
+    specific projections etc. 
+*/
+function sphereUVtoLatLon(uv: Vector2): Vector2 {
+    const lat: number = (uv.y - 0.5) * -180;
+    const lon: number = (uv.x - 0.5) * 360;
+    return new Vector2(lon, lat)
+}
+
 function main() {
-    const loader = new GLTFLoader()
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-    
+    // scene objects
+    const gltfLoader = new GLTFLoader()
     const cubeLoader = new THREE.CubeTextureLoader();
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.05, 1000 );
+    const raycaster = new THREE.Raycaster();
+    let planetScene: GLTF;
+    let mouse = new THREE.Vector2();
+    
     const skybox = cubeLoader.load([
       'assets/skybox/right.png',
       'assets/skybox/left.png',
@@ -19,10 +37,20 @@ function main() {
     ]);
     scene.background = skybox;
 
-    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.75);
     scene.add(ambientLight);
     const directionalLight = new THREE.DirectionalLight( 0xffffff, 1.5 );
-    scene.add( directionalLight );
+    scene.add(directionalLight);
+
+    // add a sphere for the atmosphere halo
+    let geometry = new THREE.SphereGeometry(0.975, 60, 30);
+    let material = new THREE.ShaderMaterial({
+        vertexShader: document.getElementById('halo-vs')!.textContent!,
+        fragmentShader: document.getElementById('halo-fs')!.textContent!,
+        transparent: true
+    })
+    let sphere = new THREE.Mesh(geometry, material);
+    scene.add(sphere);
 
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize( window.innerWidth, window.innerHeight );
@@ -35,18 +63,15 @@ function main() {
     controls.update();
     directionalLight.position.copy(camera.position);
 
-    let planetScene: GLTF;
-
-    loader.load('assets/earth.glb', (gltf: GLTF) => {
+    gltfLoader.load('assets/earth.glb', (gltf: GLTF) => {
         scene.add(gltf.scene);
         planetScene = gltf;
         console.log(scene);
     }, undefined, (errorEvent: ErrorEvent) => {
         console.error(errorEvent);
-    })
+    });
 
-    var raycaster = new THREE.Raycaster();
-    var mouse = new THREE.Vector2();
+    
 
     function onMouseMove( event: MouseEvent ) {
         mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
@@ -66,30 +91,28 @@ function main() {
 
     window.addEventListener( 'resize', onWindowResize, false );
 
-    function uvToLatLon(uv: Vector2): Vector2 {
-        const lat: number = (uv.y - 0.5) * -180;
-        const lon: number = (uv.x - 0.5) * 360;
-        return new Vector2(lon, lat)
-    }
+    window.addEventListener( 'dblclick', (ev: MouseEvent) => {
+        if(!planetScene) {
+            return;
+        }
+
+        var collisions = raycaster.intersectObjects( planetScene.scene.children, true);
+        if(collisions.length > 0) {
+            //just grab the first intersection
+            const hit = collisions[0];
+            const latLon = sphereUVtoLatLon(hit.uv!);
+
+            (document.getElementById('lon') as any).value = latLon.x;
+            (document.getElementById('lat') as any).value = latLon.y;
+        }
+    })
 
     const animate = () => {
         requestAnimationFrame( animate );
-        raycaster.setFromCamera( mouse, camera );
-
-        if(planetScene) {
-            var intersects = raycaster.intersectObjects( scene.children, true);
-            if(intersects.length > 0) {
-                //just grab the first intersection
-                const hit = intersects[0];
-                const latLon = uvToLatLon(hit.uv!);
-
-                (document.getElementById('lon') as any).value = latLon.x;
-                (document.getElementById('lat') as any).value = latLon.y;
-
-            }
-        }
+        raycaster.setFromCamera( mouse, camera );  
         
         controls.update();
+        directionalLight.position.copy(camera.position);
         renderer.render( scene, camera );
     };
 
