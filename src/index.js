03c495b1db1a2c5390622f8a8ed07ee866a63c69
Ian Overgard <ian.overgard@gmail.com>
2020-03-20 11:17:13 -0600

    Refactor to support multiple scene types

--- src/index.js
+++ src/index.js
@@ -11,9 +11,30 @@ const THREE = __importStar(require("three"));
 const OrbitControls_js_1 = require("three/examples/jsm/controls/OrbitControls.js");
 const GLTFLoader_1 = require("three/examples/jsm/loaders/GLTFLoader");
 const three_1 = require("three");
-//missing .d.ts so use the old school require syntax
-const tilebelt = require('@mapbox/tilebelt');
+//missing a .d.ts for this module so use the old school require syntax
+const TileBelt = require('@mapbox/tilebelt');
 const accessToken = 'pk.eyJ1IjoiaWFub3ZlcmdhcmQiLCJhIjoiY2s3eXpnc2VsMDB3djNsc2MyeWc0Y3BseSJ9.3BJgWc7kIFflz-t7enxvAQ';
+// threejs globals
+const gltfLoader = new GLTFLoader_1.GLTFLoader();
+const cubeLoader = new THREE.CubeTextureLoader();
+const raycaster = new THREE.Raycaster();
+let mouse = new THREE.Vector2();
+// some relevent dom objects
+const tilePreview = document.getElementById('tilepreview');
+const inputLon = document.getElementById('lon');
+const inputLat = document.getElementById('lat');
+const inputZoom = document.getElementById('zoom');
+const inputform = document.getElementById('heightfield-inputs');
+class Location {
+    constructor(lat, lon, zoom) {
+        this.lat = lat;
+        this.lon = lon;
+        this.zoom = zoom;
+    }
+}
+function currentLocation() {
+    return new Location(parseFloat(inputLat.value), parseFloat(inputLon.value), parseInt(inputZoom.value));
+}
 /*
     This is assuming a UVMap from the NASA blue-marble collection
     mapped to a standard UV sphere.
@@ -27,114 +48,185 @@ function sphereUVtoLatLon(uv) {
     const lon = (uv.x - 0.5) * 360;
     return new three_1.Vector2(lon, lat);
 }
-function loadTileFromLatLon(latLon, zoom, targetImage) {
-    const tile = tilebelt.pointToTile(latLon.x, latLon.y, zoom);
+function loadTileFromLatLonIntoImage(location, targetImage) {
+    const tile = TileBelt.pointToTile(location.lon, location.lat, location.zoom);
     const [tx, ty, tz] = tile;
     let apiUrl = `https://api.mapbox.com/v4/mapbox.terrain-rgb/${tz}/${tx}/${ty}.pngraw?access_token=${accessToken}`;
-    console.log(apiUrl);
     targetImage.src = apiUrl;
 }
+function reloadPreview() {
+    loadTileFromLatLonIntoImage(currentLocation(), tilePreview);
+}
+class GlobeScene {
+    constructor(renderer) {
+        this.active = false;
+        // scene objects
+        this.scene = new THREE.Scene();
+        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.05, 1000);
+        // start loading earth model
+        gltfLoader.load('assets/earth.glb', (gltf) => {
+            this.scene.add(gltf.scene);
+            this.planetScene = gltf;
+            console.log(this.scene);
+        }, undefined, (errorEvent) => {
+            console.error(errorEvent);
+        });
+        const skybox = cubeLoader.load([
+            'assets/skybox/right.png',
+            'assets/skybox/left.png',
+            'assets/skybox/top.png',
+            'assets/skybox/bottom.png',
+            'assets/skybox/front.png',
+            'assets/skybox/back.png',
+        ]);
+        this.scene.background = skybox;
+        this.ambientLight = new THREE.AmbientLight(0xffffff, 0.75);
+        this.scene.add(this.ambientLight);
+        this.directionalLight = new THREE.DirectionalLight(0xffffff, 1.5);
+        this.scene.add(this.directionalLight);
+        // add a sphere for the atmosphere halo
+        let geometry = new THREE.SphereGeometry(0.975, 60, 30);
+        let material = new THREE.ShaderMaterial({
+            vertexShader: document.getElementById('halo-vs').textContent,
+            fragmentShader: document.getElementById('halo-fs').textContent,
+            transparent: true
+        });
+        let sphere = new THREE.Mesh(geometry, material);
+        this.scene.add(sphere);
+        this.controls = new OrbitControls_js_1.OrbitControls(this.camera, renderer.domElement);
+        this.controls.enabled = false;
+        this.camera.position.set(0, 0, 4);
+        this.controls.update();
+        this.directionalLight.position.copy(this.camera.position);
+        const onWindowResize = () => {
+            this.camera.aspect = window.innerWidth / window.innerHeight;
+            this.camera.updateProjectionMatrix();
+        };
+        window.addEventListener('resize', onWindowResize, false);
+        renderer.domElement.addEventListener('dblclick', (ev) => {
+            if (!this.planetScene || !this.active) {
+                return;
+            }
+            var collisions = raycaster.intersectObjects(this.planetScene.scene.children, true);
+            if (collisions.length > 0) {
+                //just grab the first intersection
+                const hit = collisions[0];
+                const latLon = sphereUVtoLatLon(hit.uv);
+                inputLon.value = latLon.x.toString();
+                inputLat.value = latLon.y.toString();
+                reloadPreview();
+            }
+        });
+    }
+    setActive(active) {
+        this.active = active;
+        this.controls.enabled = active;
+    }
+    animate() {
+        raycaster.setFromCamera(mouse, this.camera);
+        this.controls.update();
+        this.directionalLight.position.copy(this.camera.position);
+    }
+    render(renderer) {
+        renderer.render(this.scene, this.camera);
+    }
+}
+class HeightmapScene {
+    constructor(renderer) {
+        this.active = false;
+        // scene objects
+        this.scene = new THREE.Scene();
+        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.05, 1000);
+        const skybox = cubeLoader.load([
+            'assets/ground-skybox/right.png',
+            'assets/ground-skybox/left.png',
+            'assets/ground-skybox/top.png',
+            'assets/ground-skybox/bottom.png',
+            'assets/ground-skybox/front.png',
+            'assets/ground-skybox/back.png',
+        ]);
+        this.scene.background = skybox;
+        this.ambientLight = new THREE.AmbientLight(0xffffff, 0.75);
+        this.scene.add(this.ambientLight);
+        this.directionalLight = new THREE.DirectionalLight(0xffffff, 1.5);
+        this.scene.add(this.directionalLight);
+        // test sphere
+        let geometry = new THREE.SphereGeometry(0.975, 60, 30);
+        let material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+        let sphere = new THREE.Mesh(geometry, material);
+        this.scene.add(sphere);
+        this.controls = new OrbitControls_js_1.OrbitControls(this.camera, renderer.domElement);
+        this.controls.enabled = false;
+        this.camera.position.set(0, 0, 4);
+        this.controls.update();
+        this.directionalLight.position.copy(this.camera.position);
+        const onWindowResize = () => {
+            this.camera.aspect = window.innerWidth / window.innerHeight;
+            this.camera.updateProjectionMatrix();
+        };
+        window.addEventListener('resize', onWindowResize, false);
+    }
+    setActive(active) {
+        this.active = active;
+        this.controls.enabled = active;
+    }
+    animate() {
+        raycaster.setFromCamera(mouse, this.camera);
+        this.controls.update();
+        this.directionalLight.position.copy(this.camera.position);
+    }
+    render(renderer) {
+        renderer.render(this.scene, this.camera);
+    }
+}
 function main() {
-    // scene objects
-    const gltfLoader = new GLTFLoader_1.GLTFLoader();
-    const cubeLoader = new THREE.CubeTextureLoader();
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.05, 1000);
-    const raycaster = new THREE.Raycaster();
-    let planetScene;
-    let mouse = new THREE.Vector2();
-    // some relevent dom objects
-    const tilePreview = document.getElementById('tilepreview');
-    const inputLon = document.getElementById('lon');
-    const inputLat = document.getElementById('lat');
-    const inputZoom = document.getElementById('zoom');
-    const inputform = document.getElementById('heightfield-inputs');
     inputform.onsubmit = (e) => {
         console.log('on submit');
         e.preventDefault();
         reloadPreview();
         return false;
     };
-    function reloadPreview() {
-        const lat = parseFloat(inputLat.value);
-        const lon = parseFloat(inputLon.value);
-        const zoom = parseInt(inputZoom.value);
-        loadTileFromLatLon(new three_1.Vector2(lon, lat), zoom, tilePreview);
-    }
     inputLon.onblur = () => { reloadPreview(); };
     inputLat.onblur = () => { reloadPreview(); };
     inputZoom.onblur = () => { reloadPreview(); };
-    const skybox = cubeLoader.load([
-        'assets/skybox/right.png',
-        'assets/skybox/left.png',
-        'assets/skybox/top.png',
-        'assets/skybox/bottom.png',
-        'assets/skybox/front.png',
-        'assets/skybox/back.png',
-    ]);
-    scene.background = skybox;
-    const ambientLight = new THREE.AmbientLight(0xffffff, 0.75);
-    scene.add(ambientLight);
-    const directionalLight = new THREE.DirectionalLight(0xffffff, 1.5);
-    scene.add(directionalLight);
-    // add a sphere for the atmosphere halo
-    let geometry = new THREE.SphereGeometry(0.975, 60, 30);
-    let material = new THREE.ShaderMaterial({
-        vertexShader: document.getElementById('halo-vs').textContent,
-        fragmentShader: document.getElementById('halo-fs').textContent,
-        transparent: true
-    });
-    let sphere = new THREE.Mesh(geometry, material);
-    scene.add(sphere);
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.setClearColor('#001122');
     document.body.appendChild(renderer.domElement);
-    const controls = new OrbitControls_js_1.OrbitControls(camera, renderer.domElement);
-    camera.position.set(0, 0, 5);
-    controls.update();
-    directionalLight.position.copy(camera.position);
-    gltfLoader.load('assets/earth.glb', (gltf) => {
-        scene.add(gltf.scene);
-        planetScene = gltf;
-        console.log(scene);
-    }, undefined, (errorEvent) => {
-        console.error(errorEvent);
-    });
     function onMouseMove(event) {
         mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
         mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
     }
     window.addEventListener('mousemove', onMouseMove, false);
-    function onWindowResize() {
-        camera.aspect = window.innerWidth / window.innerHeight;
-        camera.updateProjectionMatrix();
+    const onWindowResize = () => {
         renderer.setSize(window.innerWidth, window.innerHeight);
-    }
+    };
     window.addEventListener('resize', onWindowResize, false);
     tilePreview.onerror = () => {
         tilePreview.src = 'https://upload.wikimedia.org/wikipedia/it/3/39/Sad_mac.jpg';
     };
-    renderer.domElement.addEventListener('dblclick', (ev) => {
-        if (!planetScene) {
-            return;
+    const globeScene = new GlobeScene(renderer);
+    globeScene.setActive(true);
+    const heightmapScene = new HeightmapScene(renderer);
+    const scenes = [globeScene, heightmapScene];
+    let currentScene = globeScene;
+    const switchScene = (scene) => {
+        for (const scn of scenes) {
+            scn.setActive(false);
         }
-        var collisions = raycaster.intersectObjects(planetScene.scene.children, true);
-        if (collisions.length > 0) {
-            //just grab the first intersection
-            const hit = collisions[0];
-            const latLon = sphereUVtoLatLon(hit.uv);
-            inputLon.value = latLon.x.toString();
-            inputLat.value = latLon.y.toString();
-            reloadPreview();
-        }
-    });
+        scene.setActive(true);
+        currentScene = scene;
+    };
+    document.getElementById("view-globe-btn").onclick = () => {
+        switchScene(globeScene);
+    };
+    document.getElementById("view-heightmap-btn").onclick = () => {
+        switchScene(heightmapScene);
+    };
     const animate = () => {
         requestAnimationFrame(animate);
-        raycaster.setFromCamera(mouse, camera);
-        controls.update();
-        directionalLight.position.copy(camera.position);
-        renderer.render(scene, camera);
+        currentScene.animate();
+        currentScene.render(renderer);
     };
     animate();
 }
--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,41 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader, GLTF } from 'three/examples/jsm/loaders/GLTFLoader';
 import { Vector2 } from "three";
 
-//missing .d.ts so use the old school require syntax
-const tilebelt: any = require('@mapbox/tilebelt');
+//missing a .d.ts for this module so use the old school require syntax
+const TileBelt: any = require('@mapbox/tilebelt');
 
 const accessToken = 'pk.eyJ1IjoiaWFub3ZlcmdhcmQiLCJhIjoiY2s3eXpnc2VsMDB3djNsc2MyeWc0Y3BseSJ9.3BJgWc7kIFflz-t7enxvAQ';
+
+// threejs globals
+const gltfLoader = new GLTFLoader()
+const cubeLoader = new THREE.CubeTextureLoader();
+const raycaster = new THREE.Raycaster();
+let mouse = new THREE.Vector2();
+
+// some relevent dom objects
+const tilePreview: HTMLImageElement = document.getElementById('tilepreview') as HTMLImageElement;
+const inputLon: HTMLInputElement = document.getElementById('lon') as HTMLInputElement;
+const inputLat: HTMLInputElement = document.getElementById('lat') as HTMLInputElement;
+const inputZoom: HTMLInputElement = document.getElementById('zoom') as HTMLInputElement;
+const inputform: HTMLFormElement = document.getElementById('heightfield-inputs') as HTMLFormElement;
+
+interface ILocation {
+    lat: number
+    lon: number
+    zoom: number
+}
+
+class Location implements ILocation {
+    constructor(public lat: number, public lon: number, public zoom: number) {}
+}
+
+function currentLocation() {
+    return new Location(
+        parseFloat(inputLat.value),
+        parseFloat(inputLon.value),
+        parseInt(inputZoom.value)
+    );
+}
 
 /* 
     This is assuming a UVMap from the NASA blue-marble collection
@@ -22,30 +53,186 @@ function sphereUVtoLatLon(uv: Vector2): Vector2 {
     return new Vector2(lon, lat)
 }
 
-function loadTileFromLatLon(latLon: Vector2, zoom: number, targetImage: HTMLImageElement) {
-    const tile = tilebelt.pointToTile(latLon.x, latLon.y, zoom)
+function loadTileFromLatLonIntoImage(location: ILocation, targetImage: HTMLImageElement) {
+    const tile = TileBelt.pointToTile(location.lon, location.lat, location.zoom);
     const [tx, ty, tz] = tile;    
     let apiUrl = `https://api.mapbox.com/v4/mapbox.terrain-rgb/${tz}/${tx}/${ty}.pngraw?access_token=${accessToken}`;
-    console.log(apiUrl);
     targetImage.src = apiUrl;
 }
 
-function main() {
-    // scene objects
-    const gltfLoader = new GLTFLoader()
-    const cubeLoader = new THREE.CubeTextureLoader();
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.05, 1000 );
-    const raycaster = new THREE.Raycaster();
-    let planetScene: GLTF;
-    let mouse = new THREE.Vector2();
+function reloadPreview() {
+    loadTileFromLatLonIntoImage(currentLocation(), tilePreview);
+}
 
-    // some relevent dom objects
-    const tilePreview: HTMLImageElement = document.getElementById('tilepreview') as HTMLImageElement;
-    const inputLon: HTMLInputElement = document.getElementById('lon') as HTMLInputElement;
-    const inputLat: HTMLInputElement = document.getElementById('lat') as HTMLInputElement;
-    const inputZoom: HTMLInputElement = document.getElementById('zoom') as HTMLInputElement;
-    const inputform: HTMLFormElement = document.getElementById('heightfield-inputs') as HTMLFormElement;
+interface IScene {
+    setActive(active: boolean): any
+    animate(): any
+    render(renderer: THREE.WebGLRenderer): any
+}
+
+class GlobeScene implements IScene {
+    active: boolean = false;
+
+    scene: THREE.Scene
+    camera: THREE.PerspectiveCamera;
+    planetScene?: GLTF;
+    controls: OrbitControls;
+    directionalLight: THREE.DirectionalLight;
+    ambientLight: THREE.AmbientLight;
+
+    constructor(renderer: THREE.WebGLRenderer) {
+        // scene objects
+        this.scene = new THREE.Scene();
+        this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.05, 1000 );
+
+        // start loading earth model
+        gltfLoader.load('assets/earth.glb', (gltf: GLTF) => {
+            this.scene.add(gltf.scene);
+            this.planetScene = gltf;
+            console.log(this.scene);
+        }, undefined, (errorEvent: ErrorEvent) => {
+            console.error(errorEvent);
+        });
+         
+        const skybox = cubeLoader.load([
+            'assets/skybox/right.png',
+            'assets/skybox/left.png',
+            'assets/skybox/top.png',
+            'assets/skybox/bottom.png',
+            'assets/skybox/front.png',
+            'assets/skybox/back.png',
+          ]);
+
+        this.scene.background = skybox;
+        this.ambientLight = new THREE.AmbientLight(0xffffff, 0.75);
+        this.scene.add(this.ambientLight);
+        this.directionalLight = new THREE.DirectionalLight( 0xffffff, 1.5 );
+        this.scene.add(this.directionalLight);
+
+        // add a sphere for the atmosphere halo
+        let geometry = new THREE.SphereGeometry(0.975, 60, 30);
+        let material = new THREE.ShaderMaterial({
+            vertexShader: document.getElementById('halo-vs')!.textContent!,
+            fragmentShader: document.getElementById('halo-fs')!.textContent!,
+            transparent: true
+        })
+        let sphere = new THREE.Mesh(geometry, material);
+        this.scene.add(sphere);
+
+        this.controls = new OrbitControls( this.camera, renderer.domElement );
+        this.controls.enabled = false;
+
+        this.camera.position.set(0, 0, 4);
+        this.controls.update();
+        this.directionalLight.position.copy(this.camera.position);
+        
+        const onWindowResize = () => {
+            this.camera.aspect = window.innerWidth / window.innerHeight;
+            this.camera.updateProjectionMatrix();
+        }
+        window.addEventListener( 'resize', onWindowResize, false );
+
+        renderer.domElement.addEventListener( 'dblclick', (ev: MouseEvent) => {
+            if(!this.planetScene || !this.active) {
+                return;
+            }
+            var collisions = raycaster.intersectObjects( this.planetScene.scene.children, true);
+            if(collisions.length > 0) {
+                //just grab the first intersection
+                const hit = collisions[0];
+                const latLon = sphereUVtoLatLon(hit.uv!);
+                inputLon.value = latLon.x.toString();
+                inputLat.value = latLon.y.toString();
+                reloadPreview();
+            }
+        })
+    
+    }
+    
+    setActive(active: boolean) {
+        this.active = active;
+        this.controls.enabled = active;
+    }
+
+    animate() {
+        raycaster.setFromCamera( mouse, this.camera );        
+        this.controls.update();
+        this.directionalLight.position.copy(this.camera.position);
+    }
+
+    render(renderer: THREE.WebGLRenderer) {
+        renderer.render( this.scene, this.camera );
+    }
+}
+
+class HeightmapScene implements IScene {
+      
+    active: boolean = false;
+    scene: THREE.Scene;
+    camera: THREE.PerspectiveCamera;
+    ambientLight: THREE.AmbientLight;
+    directionalLight: THREE.DirectionalLight;
+    controls: OrbitControls;
+    
+    constructor(renderer: THREE.WebGLRenderer) {
+        // scene objects
+        this.scene = new THREE.Scene();
+        this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.05, 1000 );
+       
+         
+        const skybox = cubeLoader.load([
+            'assets/ground-skybox/right.png',
+            'assets/ground-skybox/left.png',
+            'assets/ground-skybox/top.png',
+            'assets/ground-skybox/bottom.png',
+            'assets/ground-skybox/front.png',
+            'assets/ground-skybox/back.png',
+          ]);
+
+        this.scene.background = skybox;
+        this.ambientLight = new THREE.AmbientLight(0xffffff, 0.75);
+        this.scene.add(this.ambientLight);
+        this.directionalLight = new THREE.DirectionalLight( 0xffffff, 1.5 );
+        this.scene.add(this.directionalLight);
+
+        // test sphere
+        let geometry = new THREE.SphereGeometry(0.975, 60, 30);
+        let material = new THREE.MeshBasicMaterial({color: 0x00ff00})
+        let sphere = new THREE.Mesh(geometry, material);
+        this.scene.add(sphere);
+
+        this.controls = new OrbitControls( this.camera, renderer.domElement );
+        this.controls.enabled = false;
+
+        this.camera.position.set(0, 0, 4);
+        this.controls.update();
+        this.directionalLight.position.copy(this.camera.position);
+        
+        const onWindowResize = () => {
+            this.camera.aspect = window.innerWidth / window.innerHeight;
+            this.camera.updateProjectionMatrix();
+        }
+        window.addEventListener( 'resize', onWindowResize, false );
+    }
+
+    setActive(active: boolean) {
+        this.active = active;
+        this.controls.enabled = active;
+    }
+
+    animate() {
+        raycaster.setFromCamera( mouse, this.camera );        
+        this.controls.update();
+        this.directionalLight.position.copy(this.camera.position);
+    }
+
+    render(renderer: THREE.WebGLRenderer) {
+        renderer.render( this.scene, this.camera );
+    }
+}
+
+
+function main() {
     
     inputform.onsubmit = (e: Event) => {
         console.log('on submit')
@@ -53,111 +240,61 @@ function main() {
         reloadPreview();
         return false;
     }
-
-    function reloadPreview() {
-        const lat = parseFloat(inputLat.value)
-        const lon = parseFloat(inputLon.value)
-        const zoom = parseInt(inputZoom.value);
-        loadTileFromLatLon(new Vector2(lon, lat), zoom, tilePreview);
-    }
         
     inputLon.onblur = () => { reloadPreview(); };
     inputLat.onblur = () => { reloadPreview(); };
     inputZoom.onblur = () => { reloadPreview(); };
-
-    const skybox = cubeLoader.load([
-      'assets/skybox/right.png',
-      'assets/skybox/left.png',
-      'assets/skybox/top.png',
-      'assets/skybox/bottom.png',
-      'assets/skybox/front.png',
-      'assets/skybox/back.png',
-    ]);
-    scene.background = skybox;
-
-    const ambientLight = new THREE.AmbientLight(0xffffff, 0.75);
-    scene.add(ambientLight);
-    const directionalLight = new THREE.DirectionalLight( 0xffffff, 1.5 );
-    scene.add(directionalLight);
-
-    // add a sphere for the atmosphere halo
-    let geometry = new THREE.SphereGeometry(0.975, 60, 30);
-    let material = new THREE.ShaderMaterial({
-        vertexShader: document.getElementById('halo-vs')!.textContent!,
-        fragmentShader: document.getElementById('halo-fs')!.textContent!,
-        transparent: true
-    })
-    let sphere = new THREE.Mesh(geometry, material);
-    scene.add(sphere);
-
+    
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize( window.innerWidth, window.innerHeight );
     renderer.setClearColor('#001122');
     document.body.appendChild( renderer.domElement );
-
-    const controls = new OrbitControls( camera, renderer.domElement );
-
-    camera.position.set(0, 0, 5);
-    controls.update();
-    directionalLight.position.copy(camera.position);
-
-    gltfLoader.load('assets/earth.glb', (gltf: GLTF) => {
-        scene.add(gltf.scene);
-        planetScene = gltf;
-        console.log(scene);
-    }, undefined, (errorEvent: ErrorEvent) => {
-        console.error(errorEvent);
-    });
- 
-
+    
     function onMouseMove( event: MouseEvent ) {
         mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
         mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
     }
-
     window.addEventListener( 'mousemove', onMouseMove, false );
 
-    function onWindowResize(){
-    
-        camera.aspect = window.innerWidth / window.innerHeight;
-        camera.updateProjectionMatrix();
-    
+    const onWindowResize = () => {
         renderer.setSize( window.innerWidth, window.innerHeight );
-    
     }
-
     window.addEventListener( 'resize', onWindowResize, false );
+
 
     tilePreview.onerror = () => {
         tilePreview.src = 'https://upload.wikimedia.org/wikipedia/it/3/39/Sad_mac.jpg'
     }
 
-    renderer.domElement.addEventListener( 'dblclick', (ev: MouseEvent) => {
-        if(!planetScene) {
-            return;
-        }
-
-        var collisions = raycaster.intersectObjects( planetScene.scene.children, true);
-        if(collisions.length > 0) {
-            //just grab the first intersection
-            const hit = collisions[0];
-            const latLon = sphereUVtoLatLon(hit.uv!);
-            inputLon.value = latLon.x.toString();
-            inputLat.value = latLon.y.toString();
-            reloadPreview();
-        }
-    })
-
     
-    const animate = () => {
-        requestAnimationFrame( animate );
-        raycaster.setFromCamera( mouse, camera );  
-        
-        controls.update();
-        directionalLight.position.copy(camera.position);
-        renderer.render( scene, camera );
+    const globeScene = new GlobeScene(renderer);
+    globeScene.setActive(true);
+    const heightmapScene = new HeightmapScene(renderer);
+    
+    const scenes: IScene[] = [globeScene, heightmapScene];
+    let currentScene: IScene = globeScene;
+    
+    const switchScene = (scene: IScene) => {
+        for(const scn of scenes) {
+            scn.setActive(false);
+        }
+        scene.setActive(true);
+        currentScene = scene;
+    }
+
+    document.getElementById("view-globe-btn")!.onclick = () => {
+        switchScene(globeScene);
     };
 
+    document.getElementById("view-heightmap-btn")!.onclick = () => {
+        switchScene(heightmapScene);
+    };
+
+    const animate = () => {
+        requestAnimationFrame( animate );
+        currentScene.animate();
+        currentScene.render(renderer);
+    }
     animate();
 }
  
