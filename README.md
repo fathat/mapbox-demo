483ff62cec24e982e8828d706fea24d50176ff6a
Ian Overgard <ian.overgard@gmail.com>
2020-03-20 21:28:39 -0600

    Compress earth mesh

---
+++ README.md
@@ -0,0 +1,12 @@
+# Mapbox Terrain Visualizer
+
+## Build
+
+The easiest way to build from scratch is to run these commands:
+
+    npm i
+    npm run build-dev
+    npm run start:dev
+
+This should now be hosting a server on port 8080 
+
--- src/globals.ts
+++ src/globals.ts
@@ -1,5 +1,7 @@
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
 import * as THREE from "three";
+
 
 // threejs globals
 export const gltfLoader = new GLTFLoader()
@@ -7,3 +9,7 @@ export const textureLoader = new THREE.TextureLoader();
 export const cubeLoader = new THREE.CubeTextureLoader();
 export const raycaster = new THREE.Raycaster();
 export let mouse = new THREE.Vector2();
+
+export const dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderPath( 'assets/draco/' );
+gltfLoader.setDRACOLoader( dracoLoader );
