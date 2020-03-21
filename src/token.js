40fa60f125be18c7824406a7b2ff9c9dbdfe69a9
Ian Overgard <ian.overgard@gmail.com>
2020-03-20 20:44:22 -0600

    Cleanup build process

--- src/token.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.accessToken = 'pk.eyJ1IjoiaWFub3ZlcmdhcmQiLCJhIjoiY2s3eXpnc2VsMDB3djNsc2MyeWc0Y3BseSJ9.3BJgWc7kIFflz-t7enxvAQ';
